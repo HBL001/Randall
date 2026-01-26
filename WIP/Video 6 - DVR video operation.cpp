629ae9679873e59743aaa54da3f3b2b0f4bb9cd6
richard day <richard_day@highlandbiosciences.com>
2026-01-26 18:47:16 +0000

    Changed a few values added a function call

--- WIP/Video 6 - DVR video operation.cpp
+++ WIP/Video 6 - DVR video operation.cpp
@@ -20,7 +20,6 @@ static constexpr uint8_t PIN_LED     = 6;   // D6 test LED
 static constexpr uint8_t PIN_SHUTTER = 7;   // D7 -> your shutter "press" output
 
 // Timings (ms) - adjust here
-static constexpr uint32_t T_START_UP      = 3000;   // safer than 250 ms
 static constexpr uint32_t T_SHORT_PRESS   = 500;   // safer than 250 ms
 static constexpr uint32_t T_BETWEEN       = 3000;
 static constexpr uint32_t T_RECORD_WINDOW = 10000;
@@ -29,8 +28,6 @@ static constexpr uint32_t T_LONG_PRESS    = 3000;  // safer than 1 s
 static void flashLedDuringPress(uint32_t press_ms)
 {
   // LED on for the press duration, then off
-  // Flash D6 to indicate shutter press
-
   digitalWrite(PIN_LED, HIGH);
   digitalWrite(PIN_SHUTTER, HIGH);
   delay(press_ms);
@@ -50,12 +47,6 @@ static void longPressPowerOff()
   flashLedDuringPress(T_LONG_PRESS);
 }
 
-static void longPressPowerOn()
-{
-  Serial.println(F("Long press (power on)"));
-  flashLedDuringPress(T_START_UP);
-}
-
 void setup()
 {
   pinMode(PIN_LED, OUTPUT);
@@ -70,7 +61,7 @@ void setup()
   Serial.println(F("DVR shutter timing test sequence starting..."));
 
   // 1) Long press + LED, wait 2 s
-  longPressPowerOn();
+  longPressPowerOff();
   delay(T_BETWEEN);
 
   // 2) Short press + LED, record 10 s
